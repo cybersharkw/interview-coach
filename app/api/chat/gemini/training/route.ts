99baedccee95943f47211eb58b227d8393f0c7f4
cybersharkw <jan.schwarzkopf78@gmail.com>
2025-11-13 16:23:43 +0100

    update: prompt and readme

--- app/api/chat/gemini/training/route.ts
+++ app/api/chat/gemini/training/route.ts
@@ -1,7 +1,9 @@
 import { google } from "@ai-sdk/google"
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
 
-let domain: string[] = ["Software Engineering", "Human Ressources", "Marketing"]
+// Prompts which reooccur are in lib
+import {rules, examplesTraining, coachPerson, domain} from '../../../../../lib/itcoach'
+
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -12,16 +14,13 @@ export async function POST(req: Request) {
     const result = streamText({
         model: google("gemini-2.5-flash-lite"),
         system: 
-            `Your name is AI-Coach. Your task is to test people how they are respond for a job-interview. Your personality is strong, military, and direct, but always respectfull.` +
-            `Your Questions are specific in following domain:${domain[0]}. But your Questionset must include general Questions. You ask maximal 10 Questions in each Round` +
-            
-            `# examples /n` +
-            `-system: Where are your strengths?` + 
-            `-system: Why do you want to work for us?` + 
 
-            `# rules` +
-             `If the user asks questions just go forward to the next Question of your Questionsset` + 
-             `You are not allowed to give any information out of previous conversations` 
+            `${coachPerson}` +
+            `Your Questions are specific in following domain:${domain[0]}. But your Questionset must include general Questions. You ask maximal 10 Questions in each Round. Each Question has to start with the number. ` +
+            
+            `***${examplesTraining}***` +
+
+            `***${rules}***`
     ,
         messages: convertToModelMessages(messages),
     });
--- lib/itcoach.ts
+++ lib/itcoach.ts
@@ -1,6 +1,10 @@
 export let domain: string[] = ["Software Engineering", "Human Ressources", "Marketing"]
 
-export let examplesTraining:string = ``
+export let coachPerson: string = `Your name is AI-Coach. Your task is to test people how they are respond for a job-interview. Your personality is strong, military, and direct, but always respectfull.` 
+
+export let examplesTraining:string = `# examples /n` +
+            `-system: Question 1: Where are your strengths?` + 
+            `-system: Question 2: Why do you want to work for us?`
 
 export let rules:string = `# rules` +
              `If the user asks questions just go forward to the next Question of your Questionsset` + 
